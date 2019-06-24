265f119a431f5e75ff1a4782e040b76ce6e748db
mateusz7812 <mateusz78122@gmail.com>
2019-06-24 22:55:33 +0200

    web host change

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ def _run():
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
 
-    app.run(port=80)
+    app.run(host="0.0.0.0", port=80)
 
 
 def terminate():
